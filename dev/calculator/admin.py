6db3ead59040ff3bad16724a73bd846e267ed39e
Ryan Bradley <bradleyrp@gmail.com>
2017-04-25 10:29:44 -0400

    removed deprecated django

--- dev/calculator/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Collection,Slice,Group,BackgroundCalc,Calculation
-
-admin.site.register(Collection)
-admin.site.register(Slice)
-admin.site.register(Group)
-admin.site.register(BackgroundCalc)
-admin.site.register(Calculation)
